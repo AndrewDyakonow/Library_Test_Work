b4813bdb1847d75fc840e759e89f1519d000364e
Blendi <dronramone@mail.ru>
2023-11-26 22:02:14 +0400

    Task 1.2. Create book CRUD

--- library/views.py
+++ library/views.py
@@ -1,4 +1,30 @@
-from django.utils.timezone import now
+from rest_framework import generics
+
+from library.models import Book
+from library.serializers import CRUDBookSerializer
 
 
-print(now().year)
+class CreateBookAPIView(generics.CreateAPIView):
+    serializer_class = CRUDBookSerializer
+
+
+class ListBookAPIView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = CRUDBookSerializer
+
+
+class DetailBookAPIView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = CRUDBookSerializer
+    lookup_field = 'id'
+
+
+class UpdateBookAPIView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = CRUDBookSerializer
+    lookup_field = 'id'
+
+
+class DeleteBookAPIView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    lookup_field = 'id'
